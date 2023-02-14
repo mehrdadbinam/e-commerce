3d401fa4787d73ef753bc78dcb48ed98ba1db780
mehrdadbinam <mehrdadrashedifar@gmail.com>
2023-02-14 18:24:47 +0330

    added our navigation and logo and style our main menu

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
-import Directory from './component/directory/Directory.component'
+import Home from "./routes/home/home.component.jsx";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./routes/navigation/Navigation.component";
+
+const Shop = () => {
+  return <h1>Shop page</h1>;
+};
 
 const App = () => {
-  const catagories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
-   <Directory catagories = {catagories}/>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route >
+    </Routes>
   );
 };
 
